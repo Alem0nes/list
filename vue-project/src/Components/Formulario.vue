08d293862b8535464ba9bc4bcd800e1db5bc3b6c
Alem0nes <rhernandes91@gmail.com>
2024-10-02 07:49:47 +0100

    finished todo-list

---
+++ vue-project/src/Components/Formulario.vue
@@ -0,0 +1,25 @@
+<script setup>
+const props = defineProps(['cadastrarTarefa', 'tarefaTemp', 'trocarFiltro','editaTarefaTemp'])
+
+</script>
+
+<template>
+    <form @submit.prevent="cadastrarTarefa">
+        <div class="row">
+            <div class="col">
+                <input :value="tarefaTemp" @change="editaTarefaTemp" required
+                    type="text" placeholder="Digite aqui a tarefa" class="form-control">
+            </div>
+            <div class="col-md-2">
+                <button type="submit" class="btn btn-primary">Cadastrar</button>
+            </div>
+            <div class="col-md-3">
+                <select @change="trocarFiltro" class="form-control">
+                    <option value="todas">Todas as tarefas</option>
+                    <option value="pendentes">Tarefas pendentes</option>
+                    <option value="finalizadas">Finalizadas</option>
+                </select>
+            </div>
+        </div>
+    </form>
+</template> 
